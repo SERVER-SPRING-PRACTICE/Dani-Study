a517376b56b6204bcd1db943811346e86000842a
DaEunShim <10ekdms01@naver.com>
2020-04-21 19:36:51 +0900

    study_ApplicationStartedListener

---
+++ src/main/java/me/dani/springbootgettingstarted/SampleListener2.java
@@ -0,0 +1,17 @@
+package me.dani.springbootgettingstarted;
+
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener2 implements ApplicationListener<ApplicationStartedEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
+        System.out.println("=======================");
+        System.out.println("Started");
+        System.out.println("=======================");
+    }
+    //Bean으로 등록
+}
