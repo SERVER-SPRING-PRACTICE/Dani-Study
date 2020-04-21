d697df0a08834f043080ed8d0c1a831d2feeddb0
DaEunShim <10ekdms01@naver.com>
2020-04-21 19:33:11 +0900

    study_ApplicationStartingListener

--- src/main/java/me/dani/springbootgettingstarted/SpringbootgettingstartedApplication.java
+++ src/main/java/me/dani/springbootgettingstarted/SpringbootgettingstartedApplication.java
@@ -2,12 +2,18 @@ package me.dani.springbootgettingstarted;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 @SpringBootApplication
 public class SpringbootgettingstartedApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootgettingstartedApplication.class, args);
+
+        SpringApplication app = new SpringApplication(SpringbootgettingstartedApplication.class);
+        app.addListeners(new SampleListener());
+        app.run(args);
+        // SpringApplication.run(SpringApplication.class,args);
+
     }
 
 }
